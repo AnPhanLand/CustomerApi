f1e66e88b08199df44083baae5b60ac0872ddc9c
AnPhanLand <phantruong.hoangan@gmail.com>
2026-01-13 15:48:25 +0700

    a

--- obj/Debug/net10.0/Customer.AssemblyInfo.cs
+++ obj/Debug/net10.0/Customer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1931c1172754a9c6fc75d0c48b0717e31d050225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c880fbe676eaab47ad08d7a2a51dcb9acbaa0b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
