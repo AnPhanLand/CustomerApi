34a90cf063e95ba5e4f3c69f184212593f7b2a2f
AnPhanLand <phantruong.hoangan@gmail.com>
2026-01-30 13:08:53 +0700

    a

--- src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b0e3a0a5e8b0a0f8eb97c39e499951592910e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c94431fac3a8be1545d83155577adf44b94fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
